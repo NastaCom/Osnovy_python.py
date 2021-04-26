ff010e034b812924331379e018ac1c626f76d9e0
Anastasia <gornas12345@gmail.com>
2021-04-26 18:44:14 +0300

    файлы только первого задания

--- dz1_4.py.py
+++
@@ -1,8 +0,0 @@
-
-numbers = int(input('Введи целое положительное число и я покажу в нем самую большую цифру: '))
-numb = numbers%10
-while numbers > 0:
-    if numbers%10 > numb:
-        numb = numbers%10
-    numbers = numbers//10
-print ('Самая большая цифра в твоем числе: ', numb)
